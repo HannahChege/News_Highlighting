0069be0d1a2e9ca9537a5effeb3891ecc915bb70
HannahChege <chegehannah45@gmail.com>
2018-08-30 18:30:31 +0300

    Added the config.py

--- app/views.py
+++ app/views.py
@@ -7,5 +7,22 @@ def index():
     '''
     View root page function that return the index page and its data
     '''
-    return render_template('index.html')
+    message = 'Hello world'
+    return render_template('index.html', message = message)
+
+@app.route('/source/<int:source_id>')
+def source(source_id):
+    '''
+    View source page function that returns the sourcedetails
+    '''
+    return render_template('source.html,id = source_id')
+
+def index():
+    '''
+    View root page function that return the index page and its data
+    '''
+    title = 'NEWS HIGHLIGHTING' 
+    return render_template("index.html",title = title)
+
+
     
