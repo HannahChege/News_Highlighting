135f93a8bdabc9c4fb49cf22b139842461257b01
HannahChege <chegehannah45@gmail.com>
2018-08-31 15:31:19 +0300

    Updated

--- app/request.py
+++ app/request.py
@@ -1,32 +1,33 @@
 from app import app
 import urllib.request,json
-from .models import Article, Source
-Source = source.Source
+from .models import Article
+Article = article.Article
 
 
 #Getting api key
 api_key = app.config['NEWS_API_KEY']
 
-#Getting the source base url
+#Getting the Article base url
 base_url = app.config['NEWS_API_BASE_URL']
 
-def get_articles(endpoint,category,sources):
+def get_articles(category):
     '''
-    Function thar gets the json response to our url request
+    Function that gets the json response to our url request
     '''
-    get_articles_url = base_url.format(endpoint,category,source,api_key)
+    get_articles_url = base_url.format(category,api_key)
+
     with urllib.request.urlopen(get_articles_url) as url:
-        get_articles_data =url.read()
+        get_articles_data = url.read()
         get_articles_response = json.loads(get_articles_data)
 
-        articles_results = None
+        article_results = None
 
-        if get_articles_response['articles']:
-            sources_articles_list = get_sources_response['articles']
-            source_articles = process_articles(sources_articles_list)
+        if get_articles_response['results']:
+            article_results_list = get_articles_response['results']
+            article_results = process_results(article_results_list)
 
 
-    return source_articles
+    return article_results
 
 def process_articles(source_list):
     '''
--- app/views.py
+++ app/views.py
@@ -1,31 +1,19 @@
 from flask import render_template
 from app  import app
-from .request import get_source
-#Views
+from .request import get_articles
+
 @app.route('/')
 def index():
-    '''
-    View root page function that return the index page and its data
-    '''
-    #Getting  article sources
-    article_sources = get_source('bbc-news')
-    print(article_sources)
-    title ='Home - NEWS HIGHLIGHTING'
-    return render_template('index.html', title = title,bbc_news = article_sources )
 
-# @app.route('/source/<int:source_id>')
-# def source(source_id):
-#     '''
-#     View source page function that returns the sourcedetails
-#     '''
-#     return render_template('source.html,id = source_id')
+    '''
+    View root page function that returns the index page and its data
+    '''
 
-# def index():
-#     '''
-#     View root page function that return the index page and its data
-#     '''
-#     title = 'NEWS HIGHLIGHTING' 
-#     return render_template("index.html",title = title)
+    # Getting popular movie
+    headlines_articles = get_articles('headlines')
+    print(headline_articles)
+    title = 'Home - Welcome to The best Movie Review Website Online'
+    return render_template('index.html', title = title,headlines = headlines_articles)
 
 
     
