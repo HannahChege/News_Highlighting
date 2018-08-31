251416b02fffacf44e4786efcd6fee1e4bfd72da
HannahChege <chegehannah45@gmail.com>
2018-08-31 15:33:39 +0300

    Updated

--- app/models/article.py
+++ app/models/article.py
@@ -1,9 +1,8 @@
-class Source:
+class Article:
     '''
-    Source class to define Source Objects
+    Article class to define Article Objects
     '''
 
     def __init__(self,id,name):
         self.id = id
         self.name = name
-        
