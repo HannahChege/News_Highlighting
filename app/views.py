af08825a7a3f12037c5f85dca989998f77b76786
HannahChege <chegehannah45@gmail.com>
2018-08-31 18:22:54 +0300

    Added the base html,bootsrap

--- app/views.py
+++ app/views.py
@@ -9,11 +9,15 @@ def index():
     '''
 
     # Getting headlines articles
-    headlines_articles = get_articles('sports')
-    print(headlines_articles)
+    headlines_general = get_articles('general')
+    headlines_sports = get_articles('sports')
+    headlines_entertainment = get_articles('entertainment')
+    headlines_business = get_articles('business')
+    headlines_technology = get_articles('technology')
+   
 
     title = 'Home - Welcome to The best Movie Review Website Online'
-    return render_template('index.html', title = title, headlines_articles = headlines_articles)
+    return render_template('index.html', title = title, headlines_general = headlines_general,headlines_sports = headlines_sports,headlines_entertainment = headlines_entertainment,headlines_business = headlines_business,headlines_technology = headlines_technology)
 
 
     
