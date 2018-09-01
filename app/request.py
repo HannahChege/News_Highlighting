91c0e2a78d2b06da291eaf19bf70451ed5975f03
HannahChege <chegehannah45@gmail.com>
2018-09-01 22:48:36 +0300

    Added the category

--- app/request.py
+++ app/request.py
@@ -48,25 +48,24 @@ def process_articles(source_list):
         source_articles.append(source_object)
     return source_articles
 
-def get_article1(id):
-      '''
-    Function that gets the json response to our url request
-    '''
-    get_articles1_url = https://newsapi.org/v2/top-headlines?sources={}&apiKey={}.format(id,api_key)
-    
-    with urllib.request.urlopen(get_articles1_url) as url:
-        get_articles1_data = url.read()
-        get_articles1_response = json.loads(get_articles1_data)
+
+
+def article1(id):
+    article1_url = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'.format(id,api_key)
+    print(article1_url)
+    with urllib.request.urlopen(article1_url) as url:
+        article1_data = url.read()
+        article_response = json.loads(article1_data)
 
         article1_results = None
 
-        if get_articles1_response['articles']:
-            article1_results_list = get_articles1_response['articles']
-            article1_results = process_articles1(article1_results_list)
+        if article1_response['articles']:
+            article1_article_list = article1_response['articles']
+            article1_results = process_articles1_results(article_article_list)
 
 
-    return article1_results   
-def process_articles1(article1_list):
+    return article_source_results
+def process_articles1(article_list):
     '''
     Function that processes the article1 articles and transforn them to a list of Object
 
@@ -75,8 +74,8 @@ def process_articles1(article1_list):
     Returns :
     article1_articles: A list of article1 objects\
     '''
-    article1_articles = []
-    for article1_item in article1_list:
+    article_articles1 = []
+    for article1_item in article_list:
         author = article1_item.get('author')
         title = article1_item.get('title')
         description = article1_item.get('description')
@@ -84,6 +83,6 @@ def process_articles1(article1_list):
         image = article1_item.get('urlToImage')
         time = article1_item.get('publishedAt')
 
-        article1_object = Article(author,title,description,url,image,time)
-        article1_articles1.append(article1_object)
-    return article1_articles1    
+        article_object = Article(author,title,description,url,image,time)
+        article_articles1.append(article_object)
+    return article_articles1
