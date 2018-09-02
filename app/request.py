a3a21dc3605bdfaa9e765f771784787445656982
HannahChege <chegehannah45@gmail.com>
2018-09-02 07:01:27 +0300

    The get source is not working

--- app/request.py
+++ app/request.py
@@ -1,21 +1,22 @@
 from app import app
-import urllib.request,json
+import urllib.request, json
 from .models import article
+
 Article = article.Article
 
+# Getting api key
+api_key = app.config['NEWS_API_KEY']
 
-#Getting api key
-api_key=app.config['NEWS_API_KEY']
+# Getting the Article base url
+base_url = app.config['NEWS_API_BASE_URL']
 
-#Getting the Article base url
-base_url=app.config['NEWS_API_BASE_URL']
 
 def get_articles(category):
     '''
     Function that gets the json response to our url request
     '''
-    get_articles_url = base_url.format(category,api_key)
-    
+    get_articles_url = base_url.format(category, api_key)
+
     with urllib.request.urlopen(get_articles_url) as url:
         get_articles_data = url.read()
         get_articles_response = json.loads(get_articles_data)
@@ -26,14 +27,12 @@ def get_articles(category):
             article_results_list = get_articles_response['sources']
             article_results = process_articles(article_results_list)
 
-
     return article_results
 
 
 def process_articles(source_list):
     '''
     Function that processes the source articles and transforn them to a list of Object
-
     Args:
     source_list: A list of dictionaries that contain source details
     Returns :
@@ -44,45 +43,46 @@ def process_articles(source_list):
         id = source_item.get('id')
         name = source_item.get('name')
         category = source_item.get('category')
-        source_object = Article(id,name,category)
+        source_object = Article(id, name, category)
         source_articles.append(source_object)
     return source_articles
 
+def article(id):
+    article_url = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'.format(id,api_key)
+    print(article_url)
+    with urllib.request.urlopen(article_url) as url:
+        article_data = url.read()
+        article_response = json.loads(article_data)
+
+        article_results = None
+
+        if article_response['articles']:
+            article_article_list = article_response['articles']
+            article_results = process_articles_results(article_article_list)
 
 
-def article1(id):
-    article1_url = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'.format(id,api_key)
-    print(article1_url)
-    with urllib.request.urlopen(article1_url) as url:
-        article1_data = url.read()
-        article_response = json.loads(article1_data)
-
-        article1_results = None
-
-        if article1_response['articles']:
-            article1_article_list = article1_response['articles']
-            article1_results = process_articles1_results(article_article_list)
-
-
-    return article_source_results
-def process_articles1(article_list):
+    return article_results
+def process_articles(news):
     '''
     Function that processes the article1 articles and transforn them to a list of Object
 
     Args:
-   article1_list: A list of dictionaries that contain sarticle1 details
+   article1_list: A list of dictionaries that contain article1 details
     Returns :
     article1_articles: A list of article1 objects\
     '''
-    article_articles1 = []
-    for article1_item in article_list:
-        author = article1_item.get('author')
-        title = article1_item.get('title')
-        description = article1_item.get('description')
-        url = article1_item.get('url')
-        image = article1_item.get('urlToImage')
-        time = article1_item.get('publishedAt')
+    article_results = []
+    for article in news:
+        author = article.get('author')
+        title = article.get('title')
+        description = article.get('description')
+        url = article.get('url')
+        image = article.get('urlToImage')
+        time = article.get('publishedAt')
 
-        article_object = Article(author,title,description,url,image,time)
-        article_articles1.append(article_object)
-    return article_articles1
+
+        if url:
+
+            article_objects = Article(author,title,description,url,image,time)
+            article_results.append(article_objects)
+    return article_results
--- app/views.py
+++ app/views.py
@@ -14,37 +14,38 @@ def index():
     headlines_entertainment = get_articles('entertainment')
     headlines_business = get_articles('business')
     headlines_technology = get_articles('technology')
-   
+
 
     title = 'Home - news'
     return render_template('index.html', title = title, headlines_general = headlines_general,headlines_sports = headlines_sports,headlines_entertainment = headlines_entertainment,headlines_business = headlines_business,headlines_technology = headlines_technology)
 
-@app.route('/article1/<article_id>')
-def article1(article1_id):
+@app.route('/article_<id>')
+def article(id):
     '''
-    View  page function that returns the article1 page and its data
-    '''
-
-    # Getting headlines articles
-    article1= get_articles1('article1_id')
-    print(article1)
-
-    title = f'{article1_id}'
-    return render_template('article.html',id = article ,title = title)
-
-@app.route('/category/<cat_name>')
-def category(cat_name):
-    '''
-    function that returns the category page and its data
+    View  page function that returns the article page and its data
     '''
 
     # Getting headlines articles
-    category= get_category('cat_name')
-    print(category)
+    articles = article(id)
+    # print(article)
 
-    title = f'{cat_name}'
-    return render_template('category.html',name= category ,title = title)
+    # title = 'Articles'
+    articles = article(id)
+    return render_template('article.html',articles = articles ,id = id)
 
+# @app.route('/category/<cat_name>')
+# def category(cat_name):
+#     '''
+#     function that returns the category page and its data
+#     '''
+#
+#     # Getting headlines articles
+#     category= get_category('cat_name')
+#     print(category)
+#
+#     title = f'{cat_name}'
+#     return render_template('category.html',name= category ,title = title)
+#
 
 
     
