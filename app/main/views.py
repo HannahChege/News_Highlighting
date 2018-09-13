8e34f231d218f8105b36b854517e0a615e01309b
HannahChege <chegehannah45@gmail.com>
2018-09-13 08:02:05 +0300

    Added flask

--- app/main/views.py
+++ app/main/views.py
@@ -11,21 +11,18 @@ def index():
     View root page function that returns the index page and its data
     '''
     sources= get_sources('general')
-    sources= get_sources('sport')
-    sources= get_sources('business')
-    sources= get_sources('entertainment')
-    sources= get_sources('technology')
     
     
-   return render_template('index.html', title = title, sources_general = sources_general,sources_sports = sources_sports,sources_entertainment = sources_entertainment,
-   sources_business = sources_business,sources_technology = sources_technology)
+    title = 'Home - TOP NEWS SOURCES'
+    return render_template('index.html', title = title,sources= sources)
+
 # @main.route('/articles/<source_id>')
 # def articles(source_id):
 #     '''
 #     View  page function that returns the article1 page and its data
 #     '''
 
-#     # Getting sources articles
+#     # Getting headlines articles
 #     art = get__source(source_id)
 
 #      #title = f'{Article.title}'
